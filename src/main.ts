e92a3d060105aaf80fcb2c6cc74f5f5828669ae3
Ibuki Suzuki <ibuki.nakamura@gmail.com>
2021-03-15 09:21:52 +0900

    Add Buefy

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import Vue from "vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+import "@mdi/font/css/materialdesignicons.css";
+
 import App from "./App.vue";
 import router from "./router";
 
 Vue.config.productionTip = false;
 
+Vue.use(Buefy);
 new Vue({
   router,
   render: h => h(App)
